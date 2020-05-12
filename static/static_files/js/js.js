1280a9e9b9ef37014576b6b9c5c41efc988f700e
BuildTools <unconfigured@null.spigotmc.org>
2020-05-12 18:28:16 +0200

    strona główna

--- static/static_files/js/js.js
+++ static/static_files/js/js.js
@@ -7,7 +7,12 @@ $(document).ready(function(){
         opacity : 1
     }, 500);
 
-    $(".nav-item").click(function(){
-        
-    });
+    if ($(window).width() > 991){
+        $('.navbar-dark .d-menu').hover(function () {
+                $(this).find('.sm-menu').first().stop(true, true).slideDown(150);
+            }, function () {
+                $(this).find('.sm-menu').first().stop(true, true).delay(120).slideUp(100);
+            });
+            }
+    
 });
